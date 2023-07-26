6dc9a4b1cc3480f59c93ede8d4a7ba8b0131b2d7
sharada-luitel <luitelsharada866@gmail.com>
2023-07-26 21:28:06 +0545

    api finished

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
     'home',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ home/api_urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+from .api_views import *
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'products', ProductViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('product_filter', ProductListView.as_view(), name='product_filter'),
+    path('product_crud/<int:pk>', ProductDetail.as_view(), name='product_crud'),
+]
